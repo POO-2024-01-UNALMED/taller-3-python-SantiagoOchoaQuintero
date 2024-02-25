c9cb68cfc777f237f40fa6f2f6bf2b5a3e7bfb6f
SantiagoOchoaQuintero <sochoaqu@unal.edu.co>
2024-02-25 17:33:56 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -8,7 +8,7 @@ class TV:
          self._precio = 500
          self._numTV = 1
 
-    def setNumTV(self, numTV):
+    def setNumTV(numTV, self = TV):
         self._numTV = numTV
 
     def getNumTV(self):
